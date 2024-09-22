38a9b0880306a1870ed75e5d7e3ea488b1add5bf
William Lee <will.maps@gmail.com>
2024-09-21 23:42:56 -0400

    Add specialized opportunity path utilizing base path

--- force-app/main/default/lwc/basePath/basePath.js
+++ force-app/main/default/lwc/basePath/basePath.js
@@ -1,18 +1,24 @@
 import { api, LightningElement } from 'lwc';
 
 export default class BasePath extends LightningElement {
+    @api currentStep = 'Contacted';
     @api pathItems = [
-        { label: 'Contacted', value: 'Contacted', isCurrent: true, isActive: true },
-        { label: 'Open', value: 'Open', isComplete: false },
-        { label: 'Unqualified', value: 'Unqualified', isComplete: false },
-        { label: 'Nurturing', value: 'Nurturing', isComplete: false },
-        { label: 'Closed', value: 'Closed', isComplete: false },
+        { label: 'Contacted', value: 'Contacted' },
+        { label: 'Open', value: 'Open' },
+        { label: 'Unqualified', value: 'Unqualified' },
+        { label: 'Nurturing', value: 'Nurturing' },
+        { label: 'Closed', value: 'Closed' },
     ];
 
     isCoachingExpanded = false;
 
     get coachingDetailsButtonIcon() {
         return this.isCoachingExpanded ? 'utility:chevrondown' : 'utility:chevronright';
+    }
+
+    handleStepFocus(event) {
+        console.log('stepfocus', event.detail);
+        this.dispatchEvent(new CustomEvent('stepfocus', { detail: event.detail }));
     }
 
     toggleCoachingDetails() {
---
+++ force-app/main/default/lwc/opportunityPath/opportunityPath.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+
+export default class OpportunityPath extends LightningElement {
+    pathItems = [
+        { label: 'Prospecting', value: 'Prospecting' },
+        { label: 'Qualified', value: 'Qualified' },
+        { label: 'Needs Analysis', value: 'Needs Analysis' },
+        { label: 'Value Proposition', value: 'Value Proposition' },
+        { label: 'Id. Decision Makers', value: 'Id. Decision Makers' },
+        { label: 'Perception Analysis', value: 'Perception Analysis' },
+        { label: 'Proposal/Price Quote', value: 'Proposal/Price Quote' },
+        { label: 'Negotiation/Review', value: 'Negotiation/Review' },
+        { label: 'Closed', value: 'Closed' },
+    ];
+
+    currentStep = 'Prospecting';
+
+    handleStepFocus(event) {
+        console.log('stepfocus', event.detail);
+        this.currentStep = this.pathItems[event.detail.index].value;
+    }
+}
