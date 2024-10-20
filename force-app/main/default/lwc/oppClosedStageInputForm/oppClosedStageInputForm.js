80669d20f759e5398291bbe1e9fcce32995a03b3
William Lee <will.maps@gmail.com>
2024-10-20 00:13:19 -0400

    add closed stage component picker

--- force-app/main/default/lwc/oppClosedStageInputForm/oppClosedStageInputForm.js
+++ force-app/main/default/lwc/oppClosedStageInputForm/oppClosedStageInputForm.js
@@ -1,5 +1,33 @@
-import { api, LightningElement } from 'lwc';
+import getClosedOpportunityStages from '@salesforce/apex/OpportunityPathController.getClosedOpportunityStages';
+import { api, LightningElement, wire } from 'lwc';
 
 export default class OppClosedStageInputForm extends LightningElement {
     @api recordId;
+
+    closedStageOptions;
+
+    selectedStage;
+
+    get isClosedWon() {
+        return this.selectedStage === 'Closed Won';
+    }
+
+    @wire(getClosedOpportunityStages)
+    processGetClosedOpportunityStages({ error, data }) {
+        if (data) {
+            this.closedStageOptions = data.map((closedOpportunityStage) => {
+                return {
+                    label: closedOpportunityStage.ApiName,
+                    value: closedOpportunityStage.ApiName,
+                };
+            });
+        }
+        if (error) {
+            console.error('error', error);
+        }
+    }
+
+    handleStageChange(event) {
+        this.selectedStage = event.detail.value;
+    }
 }
